7f3c902599ff00b6b690e2505ab3ab252f203847
guanghui-ren <guanghui.ren@ele.me>
2018-03-09 17:46:04 +0800

    3.3.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 /*eslint-env node */
+const path = require('path')
+
 module.exports = Object.assign({
   entry: './src',
   output: {
     library: 'VueSticky',
     libraryTarget: 'umd',
     filename: 'vue-sticky.js',
-    path: './dist',
+    path: path.resolve(__dirname, 'dist'),
   },
   externals: {
     'vue': 'Vue',
